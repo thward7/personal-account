1b73c33b76b264d245b0f375eee700be7de1bc06
thward7 <7thwardmgmt@gmail.com>
2024-06-19 17:30:59 +0500

    feat: completed calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,17 +4,28 @@ const Calendar = () => {
   const currentYear = date.getFullYear()
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate()
 
+  const getDayOfWeek = (date: Date) => {
+    const dayOfWeek = date.getDay()
+    return dayOfWeek === 0 ? 6 : dayOfWeek - 1
+  }
+
+  const firstDayOfMonth = getDayOfWeek(new Date(currentYear, currentMonth, 1))
+
   const daysOfWeek = [
-    'Sunday',
     'Monday',
     'Tuesday',
     'Wednesday',
     'Thursday',
     'Friday',
     'Saturday',
+    'Sunday',
   ]
 
-  const daysOfMonth = [...Array(daysInMonth).keys()].map((day) => day + 1)
+  const emptyDays = Array(firstDayOfMonth).fill(null)
+  const daysOfMonth = [...emptyDays, ...Array(daysInMonth).keys()].map((day) =>
+    day === null ? null : day + 1,
+  )
+
   const days = [...daysOfWeek, ...daysOfMonth]
 
   const months = [
@@ -45,10 +56,8 @@ const Calendar = () => {
           <div className='flex space-x-2'>
             <div className='h-auto w-1.5 rounded-lg bg-own-red'></div>
             <div className='flex-col'>
-              <div className='cursor-pointer font-sfprobold'>
-                Task: name of task
-              </div>
-              <div className='cursor-pointer font-sfproregular text-own-graphit text-opacity-50'>
+              <div className='font-sfprobold'>Task: name of task</div>
+              <div className='font-sfproregular text-own-graphit text-opacity-50'>
                 09:00
               </div>
             </div>
@@ -56,12 +65,12 @@ const Calendar = () => {
         </div>
         <button
           type='submit'
-          className='button-element w-1/3 bg-black bg-opacity-55 py-1 text-sm text-white transition-all hover:bg-own-blue'
+          className='button-element w-1/3 bg-black bg-opacity-55 py-1 text-sm text-white transition-all hover:bg-own-blue max-[600px]:hidden'
         >
           Add task
         </button>
       </div>
-      <div className='flex flex-col space-y-4'>
+      <div className='flex flex-col space-y-4 max-[600px]:hidden'>
         <div className='font-sfpromedium uppercase text-own-blue'>
           {months[currentMonth]}
         </div>
@@ -70,7 +79,7 @@ const Calendar = () => {
             {days.map((day, index) => (
               <div
                 key={index}
-                className={`flex size-7 items-center justify-center p-1 ${index < 7 ? 'text-own-graphit text-opacity-50' : 'cursor-pointer transition-all hover:text-own-red'} ${day === date.getDate() && 'rounded-full bg-own-blue text-white hover:scale-[1.13] hover:text-white'}`}
+                className={`flex size-7 items-center justify-center p-1 ${index < 7 ? 'text-own-graphit text-opacity-50' : 'transition-all hover:text-own-red'} ${day === date.getDate() && 'rounded-full bg-own-blue text-white hover:scale-[1.13] hover:text-white'}`}
               >
                 {index < 7 ? String(day).charAt(0) : day}
               </div>
